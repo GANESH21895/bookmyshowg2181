c3cb18107fb762636604de1e4ea8d579a7c3a2bc
GANESH VG <102243680+GANESH21895@users.noreply.github.com>
2022-12-06 18:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 let express = require("express");
 let cors = require("cors");
 let bodyparser = require("body-parser");
-let router = require("./router/router.js");
+let router = require("./router/router");
 let mongoose = require("mongoose");
 let app = express();
 app.use(cors());
 app.use(bodyparser.json());
+require("dotenv").config();
+
+const { MONGODB } = process.env;
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+})
+.then(()=>console.log('Connected to db'))
+.catch((err)=> console.log("DB connection error",err));
+
 
 
 app.use("/", router);
 
-mongoose.connect("mongodb+srv://GANESH:GAnesh21895aug1994@cluster0.85lrpkd.mongodb.net/?retryWrites=true&w=majority/BOOKMYSHOW");
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+
 
 app.get("/", function (req, res) {
-  res.send("welcome to moviesworld");
+  res.send("Welcome To Book My Show");
+  
 });
-app.listen(8000, () => {
-  console.log("listening on ", 8000);
-});
+app.listen(process.env.PORT ||8000)
